77988a863aca23a3e7ef46a872d7aaf67f847e8a
Andi Rosenhave - Daddy Lancer <andi.rosenhave@gmail.com>
2018-03-18 20:34:26 +0100

    Add project files.

---
+++ Keypad Helper/Form1.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnNum0 = New System.Windows.Forms.Button()
+        Me.btnNumLock = New System.Windows.Forms.Button()
+        Me.btnNumDec = New System.Windows.Forms.Button()
+        Me.btnNum8 = New System.Windows.Forms.Button()
+        Me.btnNum7 = New System.Windows.Forms.Button()
+        Me.btnNumEnter = New System.Windows.Forms.Button()
+        Me.btnNum9 = New System.Windows.Forms.Button()
+        Me.btnNumMinus = New System.Windows.Forms.Button()
+        Me.btnNum6 = New System.Windows.Forms.Button()
+        Me.btnNumAsterisk = New System.Windows.Forms.Button()
+        Me.btnNum5 = New System.Windows.Forms.Button()
+        Me.btnNum1 = New System.Windows.Forms.Button()
+        Me.btnNumDivide = New System.Windows.Forms.Button()
+        Me.btnNumPlus = New System.Windows.Forms.Button()
+        Me.btnNum4 = New System.Windows.Forms.Button()
+        Me.btnNum2 = New System.Windows.Forms.Button()
+        Me.btnNum3 = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.chkExcel = New System.Windows.Forms.RadioButton()
+        Me.chkNotepad = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnNum0)
+        Me.GroupBox1.Controls.Add(Me.btnNumLock)
+        Me.GroupBox1.Controls.Add(Me.btnNumDec)
+        Me.GroupBox1.Controls.Add(Me.btnNum8)
+        Me.GroupBox1.Controls.Add(Me.btnNum7)
+        Me.GroupBox1.Controls.Add(Me.btnNumEnter)
+        Me.GroupBox1.Controls.Add(Me.btnNum9)
+        Me.GroupBox1.Controls.Add(Me.btnNumMinus)
+        Me.GroupBox1.Controls.Add(Me.btnNum6)
+        Me.GroupBox1.Controls.Add(Me.btnNumAsterisk)
+        Me.GroupBox1.Controls.Add(Me.btnNum5)
+        Me.GroupBox1.Controls.Add(Me.btnNum1)
+        Me.GroupBox1.Controls.Add(Me.btnNumDivide)
+        Me.GroupBox1.Controls.Add(Me.btnNumPlus)
+        Me.GroupBox1.Controls.Add(Me.btnNum4)
+        Me.GroupBox1.Controls.Add(Me.btnNum2)
+        Me.GroupBox1.Controls.Add(Me.btnNum3)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(234, 302)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'btnNum0
+        '
+        Me.btnNum0.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum0.Location = New System.Drawing.Point(6, 243)
+        Me.btnNum0.Name = "btnNum0"
+        Me.btnNum0.Size = New System.Drawing.Size(106, 50)
+        Me.btnNum0.TabIndex = 15
+        Me.btnNum0.Text = "0" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Insert"
+        Me.btnNum0.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNum0.UseVisualStyleBackColor = True
+        '
+        'btnNumLock
+        '
+        Me.btnNumLock.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumLock.Location = New System.Drawing.Point(6, 19)
+        Me.btnNumLock.Name = "btnNumLock"
+        Me.btnNumLock.Size = New System.Drawing.Size(50, 50)
+        Me.btnNumLock.TabIndex = 9
+        Me.btnNumLock.Text = "Num" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Lock"
+        Me.btnNumLock.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumLock.UseVisualStyleBackColor = True
+        '
+        'btnNumDec
+        '
+        Me.btnNumDec.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumDec.Location = New System.Drawing.Point(118, 243)
+        Me.btnNumDec.Name = "btnNumDec"
+        Me.btnNumDec.Size = New System.Drawing.Size(50, 50)
+        Me.btnNumDec.TabIndex = 17
+        Me.btnNumDec.Text = "," & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Del"
+        Me.btnNumDec.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumDec.UseVisualStyleBackColor = True
+        '
+        'btnNum8
+        '
+        Me.btnNum8.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum8.Location = New System.Drawing.Point(62, 75)
+        Me.btnNum8.Name = "btnNum8"
+        Me.btnNum8.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum8.TabIndex = 7
+        Me.btnNum8.Text = "8"
+        Me.btnNum8.UseVisualStyleBackColor = True
+        '
+        'btnNum7
+        '
+        Me.btnNum7.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum7.Location = New System.Drawing.Point(6, 75)
+        Me.btnNum7.Name = "btnNum7"
+        Me.btnNum7.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum7.TabIndex = 6
+        Me.btnNum7.Text = "7" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnNum7.UseVisualStyleBackColor = True
+        '
+        'btnNumEnter
+        '
+        Me.btnNumEnter.Font = New System.Drawing.Font("TechnicBold", 8.249999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumEnter.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumEnter.Location = New System.Drawing.Point(174, 189)
+        Me.btnNumEnter.Name = "btnNumEnter"
+        Me.btnNumEnter.Size = New System.Drawing.Size(50, 106)
+        Me.btnNumEnter.TabIndex = 16
+        Me.btnNumEnter.Text = "Return"
+        Me.btnNumEnter.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumEnter.UseVisualStyleBackColor = True
+        '
+        'btnNum9
+        '
+        Me.btnNum9.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum9.Location = New System.Drawing.Point(118, 75)
+        Me.btnNum9.Name = "btnNum9"
+        Me.btnNum9.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum9.TabIndex = 8
+        Me.btnNum9.Text = "9" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnNum9.UseVisualStyleBackColor = True
+        '
+        'btnNumMinus
+        '
+        Me.btnNumMinus.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumMinus.Location = New System.Drawing.Point(174, 19)
+        Me.btnNumMinus.Name = "btnNumMinus"
+        Me.btnNumMinus.Size = New System.Drawing.Size(50, 50)
+        Me.btnNumMinus.TabIndex = 12
+        Me.btnNumMinus.Text = "-"
+        Me.btnNumMinus.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumMinus.UseVisualStyleBackColor = True
+        '
+        'btnNum6
+        '
+        Me.btnNum6.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum6.Location = New System.Drawing.Point(118, 131)
+        Me.btnNum6.Name = "btnNum6"
+        Me.btnNum6.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum6.TabIndex = 5
+        Me.btnNum6.Text = "6"
+        Me.btnNum6.UseVisualStyleBackColor = True
+        '
+        'btnNumAsterisk
+        '
+        Me.btnNumAsterisk.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumAsterisk.Location = New System.Drawing.Point(118, 19)
+        Me.btnNumAsterisk.Name = "btnNumAsterisk"
+        Me.btnNumAsterisk.Size = New System.Drawing.Size(50, 50)
+        Me.btnNumAsterisk.TabIndex = 11
+        Me.btnNumAsterisk.Text = "*"
+        Me.btnNumAsterisk.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumAsterisk.UseVisualStyleBackColor = True
+        '
+        'btnNum5
+        '
+        Me.btnNum5.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum5.Location = New System.Drawing.Point(62, 131)
+        Me.btnNum5.Name = "btnNum5"
+        Me.btnNum5.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum5.TabIndex = 1
+        Me.btnNum5.Text = "5"
+        Me.btnNum5.UseVisualStyleBackColor = True
+        '
+        'btnNum1
+        '
+        Me.btnNum1.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum1.Location = New System.Drawing.Point(6, 187)
+        Me.btnNum1.Name = "btnNum1"
+        Me.btnNum1.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum1.TabIndex = 0
+        Me.btnNum1.Text = "1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnNum1.UseVisualStyleBackColor = True
+        '
+        'btnNumDivide
+        '
+        Me.btnNumDivide.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumDivide.Location = New System.Drawing.Point(62, 19)
+        Me.btnNumDivide.Name = "btnNumDivide"
+        Me.btnNumDivide.Size = New System.Drawing.Size(50, 50)
+        Me.btnNumDivide.TabIndex = 10
+        Me.btnNumDivide.Text = "/"
+        Me.btnNumDivide.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumDivide.UseVisualStyleBackColor = True
+        '
+        'btnNumPlus
+        '
+        Me.btnNumPlus.Font = New System.Drawing.Font("TechnicBold", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNumPlus.Location = New System.Drawing.Point(174, 75)
+        Me.btnNumPlus.Name = "btnNumPlus"
+        Me.btnNumPlus.Size = New System.Drawing.Size(50, 106)
+        Me.btnNumPlus.TabIndex = 13
+        Me.btnNumPlus.Text = "+"
+        Me.btnNumPlus.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.btnNumPlus.UseVisualStyleBackColor = True
+        '
+        'btnNum4
+        '
+        Me.btnNum4.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum4.Location = New System.Drawing.Point(6, 131)
+        Me.btnNum4.Name = "btnNum4"
+        Me.btnNum4.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum4.TabIndex = 3
+        Me.btnNum4.Text = "4"
+        Me.btnNum4.UseVisualStyleBackColor = True
+        '
+        'btnNum2
+        '
+        Me.btnNum2.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum2.Location = New System.Drawing.Point(62, 187)
+        Me.btnNum2.Name = "btnNum2"
+        Me.btnNum2.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum2.TabIndex = 1
+        Me.btnNum2.Text = "2"
+        Me.btnNum2.UseVisualStyleBackColor = True
+        '
+        'btnNum3
+        '
+        Me.btnNum3.Font = New System.Drawing.Font("TechnicBold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.btnNum3.Location = New System.Drawing.Point(118, 187)
+        Me.btnNum3.Name = "btnNum3"
+        Me.btnNum3.Size = New System.Drawing.Size(50, 50)
+        Me.btnNum3.TabIndex = 2
+        Me.btnNum3.Text = "3" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnNum3.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(251, 87)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(83, 23)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start Logging"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'chkExcel
+        '
+        Me.chkExcel.AutoSize = True
+        Me.chkExcel.Location = New System.Drawing.Point(252, 57)
+        Me.chkExcel.Name = "chkExcel"
+        Me.chkExcel.Size = New System.Drawing.Size(82, 17)
+        Me.chkExcel.TabIndex = 2
+        Me.chkExcel.Text = "Excel Sheet"
+        Me.chkExcel.UseVisualStyleBackColor = True
+        '
+        'chkNotepad
+        '
+        Me.chkNotepad.AutoSize = True
+        Me.chkNotepad.Checked = True
+        Me.chkNotepad.Location = New System.Drawing.Point(252, 34)
+        Me.chkNotepad.Name = "chkNotepad"
+        Me.chkNotepad.Size = New System.Drawing.Size(60, 17)
+        Me.chkNotepad.TabIndex = 3
+        Me.chkNotepad.TabStop = True
+        Me.chkNotepad.Text = "Internal"
+        Me.chkNotepad.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(253, 116)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(81, 191)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 324)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.chkNotepad)
+        Me.Controls.Add(Me.chkExcel)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnNum0 As Button
+    Friend WithEvents btnNumLock As Button
+    Friend WithEvents btnNumDec As Button
+    Friend WithEvents btnNum8 As Button
+    Friend WithEvents btnNum7 As Button
+    Friend WithEvents btnNumEnter As Button
+    Friend WithEvents btnNum9 As Button
+    Friend WithEvents btnNumMinus As Button
+    Friend WithEvents btnNum6 As Button
+    Friend WithEvents btnNumAsterisk As Button
+    Friend WithEvents btnNum5 As Button
+    Friend WithEvents btnNum1 As Button
+    Friend WithEvents btnNumDivide As Button
+    Friend WithEvents btnNumPlus As Button
+    Friend WithEvents btnNum4 As Button
+    Friend WithEvents btnNum2 As Button
+    Friend WithEvents btnNum3 As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents chkExcel As RadioButton
+    Friend WithEvents chkNotepad As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Keypad Helper/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private WithEvents kbHook As New KeyboardHook
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub kbHook_KeyDown(Key As Keys) Handles kbHook.KeyDown
+        Debug.WriteLine(Key.ToString)
+    End Sub
+
+    Private Sub kbHook_KeyUp(Key As Keys) Handles kbHook.KeyUp
+        Debug.WriteLine(Key)
+    End Sub
+
+
+End Class
